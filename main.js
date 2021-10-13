a705cf7fbc6f8616c61eb243aab8e1cede3ccc42
leannel22 <leannel@andrew.cmu.edu>
2021-10-13 11:29:07 -0400

    Updated main.js

--- main.js
+++ main.js
@@ -1,29 +1,45 @@
-function bird(name, age) {
-    this.name = name;
-    this.age = age;
-    this.image_alt = "A brown bird.";
-    this.image = "bird.jpeg";
-  }
+function Bird(name, age) {
+  this.name = name;
+  this.age = age;
+  this.image_alt = "A brown bird.";
+  this.image = "bird.jpeg";
+}
 
 
-  function hedgehog(name, age) {
-    this.name = name;
-    this.age = age;
-    this.image_alt = "A curled up hedgehog in a field of flowers.";
-    this.image = "hedgehog.jpeg";
-  }
+function Hedgehog(name, age) {
+  this.name = name;
+  this.age = age;
+  this.image_alt = "A curled up hedgehog in a field of flowers.";
+  this.image = "hedgehog.jpeg";
+}
 
-  function bunny(name, age) {
-    this.name = name;
-    this.age = age;
-    this.image_alt = "A brown bunny in the grass.";
-    this.image = "bunny.jpeg";
-  }
+function Bunny(name, age) {
+  this.name = name;
+  this.age = age;
+  this.image_alt = "A brown bunny in the grass.";
+  this.image = "bunny.jpeg";
+}
 
 
-  const animal = [bird, hedgehog, bunny];
-  const names = ["Jimothy","Trevor","Penelope","Chip"];
+const animal = [Bird, Hedgehog, Bunny];
+const names = ["Jimothy","Penelope","Chip"];
 
-  function generateRandomIndex(maxIndex) {
-    console.log(Math.floor(Math.random()*maxIndex);)
+function generateRandomIndex(maxIndex) {
+  const randomIndex = Math.floor(Math.random()*maxIndex);
+  console.log(randomIndex);
+}
+
+function generateRandomName() {
+  const randomNameIdx = generateRandomIndex(3);
+  console.log(randomNameIdx + " " + names[randomNameIdx]);
+}
+
+function generateRandomAge() {
+  const randomIndex = generateRandomIndex(7);
+  this.name = names[randomIndex];
+}
+
+function generateRandomAnimal() {
+  const randomIdx = generateRandomIndex(3);
+  this.animal = animal[randomIdx];
 }
